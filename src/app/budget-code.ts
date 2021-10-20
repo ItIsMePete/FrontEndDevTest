2f08531420f4581b517e62f98087e360a045371e
ItIsMePete <76666652+ItIsMePete@users.noreply.github.com>
2021-10-20 05:59:06 -0400

    Final budget-code.ts

---
+++ src/app/budget-code.ts
@@ -0,0 +1,16 @@
+export interface BudgetCode{
+    results: string
+    data: [
+        budgetCodeId: number,
+        fiscalYear: number,
+        budgetCode: string,
+        budgetTitle: string,
+    ]
+}
+
+/* export interface BudgetCode{
+    budgetCodeId: number
+    fiscalYear: number
+    budgetCode: string
+    budgetTitle: string
+} */
