e7de96138a2bf7cda266a1ddbdcdaa2656682220
ItIsMePete <76666652+ItIsMePete@users.noreply.github.com>
2021-10-20 00:46:20 -0400

    Added data checking methods
    Added validateResponse() function to check if api data was usable or not
    Added error throwing to httpclient get function call

---
+++ src/app/fiscal-year-list/fiscal-year-list.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../get-api-data.service';
+import { BudgetCode } from '../budget-code';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-fiscal-year-list',
+  templateUrl: './fiscal-year-list.component.html',
+  styleUrls: ['./fiscal-year-list.component.css']
+})
+export class FiscalYearListComponent implements OnInit {
+
+  budget: any;
+  /* fiscalYear: string =""; */
+  fiscalYear: number = 0;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+  }
+    /* Calls api service to get data with requested Budget ID */
+  async getSpecificFiscalYear(formValue: NgForm):Promise<void>{
+    const year = {
+      fiscalYear: formValue.value.fiscalYear
+    };
+    this.apiService.getSpecificFiscalYear(year.fiscalYear).subscribe(data => {
+      this.budget = data;;
+    }, (err) => {
+      console.log("Unable to get data from URL " + err)
+    });
+
+  }
+
+  /* Validate if api resonse has data */
+  validateResponse(){
+    console.log("validate Response");
+    if(this.budget.results==="Success"){
+      /* this.response = true; */
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  /* Testing function */
+  tester(n: string): void{
+    /* this.apiService.tester(); */
+    console.log("Testing Method component");
+    this.apiService.tester(n).subscribe(data => {
+      console.log("check 3: " + data);
+      this.budget = data;
+    })
+  }
+}
+
